c9f7fb25c031ade071711a4ae23d480e643a374e
Denperidge <denperidge@gmail.com>
2022-12-13 12:38:34 +0100

    Updated docstrings

--- app/export.py
+++ app/export.py
@@ -4,11 +4,14 @@ from jinja2 import FileSystemLoader, Environment
 from jinja_markdown import MarkdownExtension
 
 def to_docs(types_and_repos):
+    """Exports dict(key: str(type), value=[Repo...]) into build/*.html"""
+    # Get and create needed dirs
     template_dir = join(realpath(dirname(__file__)), "templates")
     output_dir = realpath(join(dirname(__file__), "..", "build"))
 
     makedirs(output_dir, exist_ok=True)
 
+    # Setup Jinja environment & templates
     env = Environment(
         loader=FileSystemLoader(template_dir),
         autoescape=True,
@@ -22,19 +25,23 @@ def to_docs(types_and_repos):
     template_index = env.get_template("index.html")
     template_repo = env.get_template("repo.html")
 
-
+    # Create top-level index.html
     with open(join(output_dir, "index.html"), "w") as index_file:
         index_content = template_index.render(
             types_and_repos=types_and_repos
         )
         index_file.write(index_content)
     
+    # For every type (ember addons, templates...)
     for repo_type in types_and_repos:
-        repo_type_dir = join(output_dir, repo_type)
+        # Get all repos
         repos = types_and_repos[repo_type]
 
+        # Create the output dir (public/Templates)
+        repo_type_dir = join(output_dir, repo_type)
         makedirs(repo_type_dir, exist_ok=True)
 
+        # And generate a docpage based on the README for every repo in that type
         for repo in repos:
             readme = repo.get_file("README.md").text
 
